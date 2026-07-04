552275af3c1853be9a95a54cfeaf190f086c5f05
zhi-qin <2512325572@qq.com>
2026-07-04 12:44:32 +0800

    初始提交：Enhanced Mob Spawners 1.7.10 Forge 移植

--- src/main/java/com/branders/spawnermod/registry/ModRegistry.java
+++ src/main/java/com/branders/spawnermod/registry/ModRegistry.java
@@ -1,6 +1,9 @@
 package com.branders.spawnermod.registry;
 
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 import com.branders.spawnermod.item.ItemDebugHarmPotion;
 import com.branders.spawnermod.item.SpawnerKey;
@@ -18,5 +21,16 @@ public class ModRegistry {
 
         GameRegistry.registerItem(spawnerKey, "spawner_key");
         GameRegistry.registerItem(debugHarmPotion, "debug_harm_potion");
+
+        // 合成配方：2个钻石 + 1个刷怪笼 → 1个刷怪笼钥匙
+        GameRegistry.addShapedRecipe(
+            new ItemStack(spawnerKey, 1),
+            " D ",
+            " D ",
+            " S ",
+            'D',
+            Items.diamond,
+            'S',
+            Blocks.mob_spawner);
     }
 }
