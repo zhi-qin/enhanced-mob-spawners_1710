95c1217bb0cd7cb1b34ed0f6017ddfec5f81fcae
zhi-qin <2512325572@qq.com>
2026-07-04 12:48:29 +0800

    清除PowerShell脚本文件，更新.gitignore

--- download_mc.ps1
+++
@@ -1,5 +0,0 @@
-param([string]$url, [string]$out)
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile($url, $out)
-Write-Output "Downloaded to $out"
